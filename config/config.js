41b6855ecaf0887dd9b3a34451f627f3e85cc14d
Ash Wilson <smashwilson@gmail.com>
2016-01-04 15:11:23 -0500

    Those are unnecessary, too.

--- config/config.js
+++
@@ -1,7 +0,0 @@
-var app = window.app;
-
-/*
-* $scope.configs, $scope.branch and $scope.pluginConfig, among others are available from the parent scope
-* */
-app.controller('GitHubAuthCtrl', ['$scope', function ($scope) {
-}]);
